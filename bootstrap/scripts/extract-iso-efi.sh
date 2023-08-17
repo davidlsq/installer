31a6aaba744a0578bb139efb2eeaa6477d2c6a69
davidlsq <contact@davidlsq.fr>
2023-08-17 14:23:30 +0200

    Add make github action

--- bootstrap/scripts/extract-iso-efi.sh
+++ bootstrap/scripts/extract-iso-efi.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-EFI_LINE=$(fdisk "$1" | grep -F "2: EF" | sed 's/.*\[\(.*\)\].*/\1/')
-EFI_START=$(echo "$EFI_LINE" | awk '{print $1}')
-EFI_SECTORS=$(echo "$EFI_LINE" | awk '{print $3}')
+if [[ "$OSTYPE" == "linux"* ]]; then
+  EFI_LINE=$(fdisk -l "$1" | grep -F "${1}2")
+  EFI_START=$(echo "$EFI_LINE" | awk '{print $2}')
+  EFI_SECTORS=$(echo "$EFI_LINE" | awk '{print $3}')
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  EFI_LINE=$(fdisk "$1" | grep -F "2: EF" | sed 's/.*\[\(.*\)\].*/\1/')
+  EFI_START=$(echo "$EFI_LINE" | awk '{print $1}')
+  EFI_SECTORS=$(echo "$EFI_LINE" | awk '{print $3}')
+else
+  exit 1
+fi
+
 dd if="$1" bs=512 skip=$EFI_START count=$EFI_SECTORS of="$2" status=none
