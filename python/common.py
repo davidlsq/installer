6518dff320a4bd5d2a6c1a129a1f2320e5086e55
davidlsq <contact@davidlsq.fr>
2023-08-25 20:26:42 +0200

    Use pathlib in python

--- python/common.py
+++ python/common.py
@@ -2,10 +2,10 @@ import yaml
 
 
 def read_yaml(path):
-    with open(path, "r") as path_stream:
+    with path.open("r") as path_stream:
         return yaml.safe_load(path_stream)
 
 
 def write_yaml(value, path):
-    with open(path, "w") as path_stream:
+    with path.open("w") as path_stream:
         yaml.dump(value, path_stream, sort_keys=False)
