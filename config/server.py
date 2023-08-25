e9190b73fc1780c7632c2fdda3380503d69ff2e6
davidlsq <contact@davidlsq.fr>
2023-08-25 20:08:44 +0200

    Rename command names in config scripts

--- config/server.py
+++ config/server.py
@@ -9,12 +9,12 @@ from ssh import ssh_config, ssh_keygen, ssh_known_host
 command = sys.argv[1]
 output = Path(sys.argv[2])
 
-if command == "configure/keys":
+if command == "keys":
     output.mkdir()
     ssh_keygen(output / "server")
     ssh_keygen(output / "user")
     ssh_keygen(output / "ansible")
-elif command == "configure/ssh":
+elif command == "ssh":
     output.mkdir()
     keys = Path(sys.argv[3])
     known_host = output / "known_host"
@@ -25,6 +25,6 @@ elif command == "configure/ssh":
     ssh_config(config, known_host, "ansible", "server.local", keys / "ansible")
     ssh_config(config, known_host, "david", "server.davidlsq.fr", keys / "user")
     ssh_config(config, known_host, "ansible", "server.davidlsq.fr", keys / "ansible")
-elif command == "configure/password":
+elif command == "password":
     password = Path(sys.argv[3])
     password_hash(password, output, ["root", "user", "ansible"])
