ce4f72a16bb58222004cd0d20c8df24dab4bbd36
davidlsq <contact@davidlsq.fr>
2023-08-29 21:26:15 +0200

    Add plex updatebot

--- .github/actions/updatebot/run.sh
+++ .github/actions/updatebot/run.sh
@@ -2,6 +2,8 @@
 
 set -e -o pipefail
 
+apt-get update
+
 tarball_nix () {
   revision=$(wget -q -O - https://channels.nixos.org/$1/git-revision | head -c 12)
   name="$1-$revision"
@@ -16,4 +18,18 @@ tarball_nix () {
 EOF
 }
 
+plex () {
+  version=$(apt-cache policy plexmediaserver | grep -F "Candidate:" | awk '{ print $2 }')
+  echo "$version"
+}
+
+rm -rf versions
+mkdir -p versions
+
 tarball_nix nixos-23.05 versions/tarball.nix
+
+cat > versions/ansible.yml << EOF
+---
+
+plex_version: $(plex)
+EOF
