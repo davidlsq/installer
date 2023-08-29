882c2d03c2651451c317edbd723df9a589094d2c
davidlsq <davidlsq@users.noreply.github.com>
2023-08-29 17:09:08 +0000

    [create-pull-request] automated change

--- versions/tarball.nix
+++ versions/tarball.nix
@@ -1,5 +1,5 @@
 {
-  name = "nixos-23.05-fc944919f743";
-  url = "https://github.com/NixOS/nixpkgs/archive/fc944919f743.tar.gz";
-  sha256 = "0kv3z80l5cd27n3yhv0f1gn5bky5a04c3ykczlqksxksppr8cihz";
+  name = "nixos-23.05-ea5234e7073d";
+  url = "https://github.com/NixOS/nixpkgs/archive/ea5234e7073d.tar.gz";
+  sha256 = "1iqfglh1fdgqxm7n4763k1cipna68sa0cb3azm2gdzhr374avcvk";
 }
