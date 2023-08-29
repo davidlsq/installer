ade5f65f47d7776a6a2c6e962e17c621dab3e97c
davidlsq <davidlsq@users.noreply.github.com>
2023-08-29 17:59:14 +0000

    [Update Bot] Apply update

--- versions/tarball.nix
+++ versions/tarball.nix
@@ -1,5 +1,5 @@
 {
-  name = "nixos-23.05-fc944919f743";
-  url = "https://github.com/NixOS/nixpkgs/archive/fc944919f743.tar.gz";
-  sha256 = "0kv3z80l5cd27n3yhv0f1gn5bky5a04c3ykczlqksxksppr8cihz";
+  name = "nixos-23.05-c540061ac8d7";
+  url = "https://github.com/NixOS/nixpkgs/archive/c540061ac8d7.tar.gz";
+  sha256 = "136l0lnbfcx2kb88hbzl0c019klr9z98synq1s0h2wn786dpqs36";
 }
