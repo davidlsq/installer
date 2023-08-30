219fc578ea2a5b64e109fdcd33fdf3d1d60f8563
davidlsq <davidlsq@users.noreply.github.com>
2023-08-30 16:28:49 +0000

    [Update Bot] Apply tarball_nix update

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 let
   nixpkgs = builtins.fetchTarball {
-    name = "nixos-23.05-ddf4688dc7ae";
-    url = "https://github.com/NixOS/nixpkgs/archive/ddf4688dc7ae.tar.gz";
-    sha256 = "1dwa763zp97jymsx2g2xky0yl7gp8nc22yrwf4n3084ahdb149b3";
+    name = "nixos-23.05-2ab91c8d65c0";
+    url = "https://github.com/NixOS/nixpkgs/archive/2ab91c8d65c0.tar.gz";
+    sha256 = "0235cvy3il52gspdfcqbc6ja9s90a9l3qk2gyyxvxnriqa70zfy2";
   };
   pkgs = import nixpkgs { };
   python-packages = p: [ p.passlib p.pyyaml p.jinja2 ];
