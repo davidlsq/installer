e39e8e6df12de92bdeca915dc16c305c61da39dc
davidlsq <contact@davidlsq.fr>
2023-10-06 18:43:07 +0200

    Rework ssh github action

---
+++ scripts/ssh-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+TMP=$(mktemp -d)
+
+USER=$(echo "$1" | cut -d '@' -f 1)
+HOST=$(echo "$1" | cut -d '@' -f 2)
+shift 1
+
+touch "$TMP/user"; chmod u=rw,go= $_
+echo "$SSH_USER_KEY" | base64 -d > "$TMP/user"
+echo "$HOST  $(echo "$SSH_HOST_KEY" | base64 -d)" > "$TMP/known_host"
+ssh "$USER@$HOST" -i "$TMP/user" -o "GlobalKnownHostsFile=$TMP/known_host" $@ || true
+
+rm -r "$TMP"
