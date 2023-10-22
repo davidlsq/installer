c30b6853e4adfe35079393918e977bff3e89fc6e
davidlsq <contact@davidlsq.fr>
2023-10-22 19:16:50 +0200

    Fix install

--- scripts/raspi/stage/00-install/03-run.sh
+++ scripts/raspi/stage/00-install/03-run.sh
@@ -1,6 +1,31 @@
 #!/bin/bash -e
 
-CRON="@reboot /install/install.sh >> /install/install.log 2>&1"
-on_chroot << EOF
-crontab -l | grep -vF "$CRON" | { cat; echo "$CRON"; } | crontab -
+source files/config
+
+if [ "$_NETWORK_MANUAL" == "true" ]; then
+cat > "${STAGE_WORK_DIR}/interfaces" <<EOF
+auto lo
+iface lo inet loopback
+
+auto $_NETWORK_INTERFACE
+allow-hotplug $_NETWORK_INTERFACE
+iface $_NETWORK_INTERFACE inet static
+  address $_NETWORK_MANUAL_ADDRESS
+  netmask $_NETWORK_MANUAL_NETMASK
+  gateway $_NETWORK_MANUAL_GATEWAY
 EOF
+echo "$_NETWORK_MANUAL_DNS" | tr ' ' '\n' | awk '{print "nameserver "$1}' \
+  > "${STAGE_WORK_DIR}/resolv.conf"
+install -m 644 "${STAGE_WORK_DIR}/interfaces" "${ROOTFS_DIR}/etc/network/interfaces"
+install -m 644 "${STAGE_WORK_DIR}/resolv.conf" "${ROOTFS_DIR}/etc/resolv.conf"
+else
+cat > "${STAGE_WORK_DIR}/interfaces" <<EOF
+auto lo
+iface lo inet loopback
+
+auto $_NETWORK_INTERFACE
+allow-hotplug $_NETWORK_INTERFACE
+iface $_NETWORK_INTERFACE inet dhcp
+EOF
+install -m 644 "${STAGE_WORK_DIR}/interfaces" "${ROOTFS_DIR}/etc/network/interfaces"
+fi
